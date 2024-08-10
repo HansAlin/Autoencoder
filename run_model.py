1151611c6bd63ab934f8095becd77c5982688157
Hans Alin <hansalin@gmail.com>
2024-08-10 12:16:26 +0200

    added ReadMe

--- run_model.py
+++ run_model.py
@@ -1,12 +1,4 @@
-import os
-from gpuutils import GpuUtils
-GpuUtils.allocate(gpu_count=1, framework='keras')
-import tensorflow as tf
-physical_devices = tf.config.list_physical_devices('GPU')
-for device in physical_devices:
-    tf.config.experimental.set_memory_growth(device, True)
-import matplotlib.pyplot as plt
-import pandas as pd
+
 
 from creating_models import load_models
 import creating_models as cm
